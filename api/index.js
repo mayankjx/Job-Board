1094cd5a460b3ce5aa8338799e5bf035c0307a2d
mayankjx <prototype.mayank@gmail.com>
2022-07-02 11:05:44 +0530

    initial setup

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+// Create express instance
+const app = express()
+
+module.exports = app
+
+// Start standalone server if directly running
+if (require.main === module) {
+  const port = process.env.PORT || 3001
+  app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`API server listening on the port ${port}`)
+  })
+}
